ce048b3e4c46b2bd48c68068e80198b1c32d686b
Shawn Renner <shawn.renner@gmail.com>
2024-09-07 11:06:30 -0500

    setup checkpoint

---
+++ client/src/components/InteractiveGame.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <p>Select the door you think contains the prize.</p>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    Door #1
+                </div>
+                <img src="../assets/door-closed-fill.svg" class="" />
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    Door #2
+                </div>
+                <img src="../assets/door-closed-fill.svg" class="" />
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    Door #3
+                </div>
+                <img src="../assets/door-closed-fill.svg" class="" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
+  
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import InteractiveGame from '@/components/InteractiveGame.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <p>This is a game where you guess which door contains a prize. Would you like to play?</p>
+    <InteractiveGame></InteractiveGame>
+    <p>Interesting choice. Before we reveal the winning door, I am going to show you one of the doors you <span class="">didn't</span> choose.</p>
+    <p>There is one more step to the game. I am giving you the opportunity to change your answer. Would you like to select the other door? Think carefully!</p>
+    
+    <p>Believe it or not, you have a better chance to win the game if you always switch doors.</p>
+    <p>Don't believe me? I didn't believe it either at first. Let's automate the game to play it hundreds of times, and see how often it's to the player's advantage to switch doors.</p>
   </main>
 </template>
