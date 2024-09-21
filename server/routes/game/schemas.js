9db26e6bdb1d08639f43b8bc7b86827f32e94078
Shawn Renner <shawn.renner@gmail.com>
2024-09-21 16:38:56 -0500

    start game schema

---
+++ server/routes/game/schemas.js
@@ -0,0 +1,62 @@
+'use strict'
+
+const game = {
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "$id": "https://montyhall.dev/game.schema.json",
+    "title": "Game",
+    "description": "Instance of a Monty Hall game",
+    "type": "object",
+    "properties": {
+        "id": {
+            "description": "PK (Autoincrement in db)",
+            "type": "integer"
+        },
+        "webid": {
+            "description": "Player-facing unique identifier",
+            "type": "string"
+        },
+        "initialDoor": {
+            "description": "Initial door the player chose",
+            "type": "integer",
+            "minimum": 0,
+            "maximum": 2
+        },
+        "winningDoor": {
+            "description": "The door with the prize",
+            "type": "integer",
+            "minimum": 0,
+            "maximum": 2
+        },
+        "hostDoor": {
+            "description": "The door the host shows to the user",
+            "type": "integer",
+            "minimum": 0,
+            "maximum": 2
+        },
+        "humanPlayer": {
+            "description": "true for interactive game, false for automated",
+            "type": "boolean"
+        },
+        "switchDoor": {
+            "description": "true if the user decided to switch doors",
+            "type": "boolean"
+        },
+        "dateStart": {
+            "description": "Timestamp when game was initialized",
+            "type": "string"
+        },
+        "dateEnd": {
+            "description": "Timestamp for when all game activities are over",
+            "type": "string"
+        },
+        "ip": {
+            "description": "ip of user that initiated the game",
+            "type": "string"
+        }
+    },
+    "$comment": "TODO: set required fields"
+}
+
+module.exports = {
+  game
+}
